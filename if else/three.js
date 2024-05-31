a39e97aa801cf00eae1f26035b1b9f609e92fccc
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2024-05-31 13:58:49 +0530

    exe

---
+++ if else/three.js
@@ -0,0 +1,17 @@
+var score = 70
+
+if(score<=50)
+{
+    console.log("you need to improve")
+}
+
+if(score>50 && score<=70)
+{
+    console.log("Good Job")
+}
+
+if(score>70)
+{
+    console.log("Excellent performance")
+}
+
---
+++ if else/two.js
@@ -0,0 +1,13 @@
+color = "red"
+if (color ==   "red")
+{
+    console.log("stop")
+}
+if (color = "yellow")
+{
+    console.log("get ready")
+}
+else
+{
+    console.log("go")
+}
