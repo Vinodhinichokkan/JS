3367a9346d2fe6e28d3b340192ab42cf5f871bd5
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2024-06-04 13:54:36 +0530

    exe

---
+++ math/one.js
@@ -0,0 +1,8 @@
+var a = Math.random()
+
+console.log(a)
+
+
+var a = Math.random()
+
+console.log(a*10)
---
+++ math/two.js
@@ -0,0 +1,6 @@
+var b =Math.floor(6.77)
+console.log(b)  //6
+
+var a = Math.random()
+
+console.log(Math.floor(a*10))
